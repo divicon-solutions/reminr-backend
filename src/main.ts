708f3316ca58f1e2ba60656f20d1e467b763cfe3
Sriram52 <nyshadhamramu@gmail.com>
2024-03-29 15:41:08 -0400

    Update configuration and enable versioning in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe, VersioningType } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import * as admin from 'firebase-admin';
@@ -25,7 +25,7 @@ function initializeFirebase() {
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { logger: console });
   const logger = new Logger('Main');
-  app.setGlobalPrefix('api/v1');
+  app.enableVersioning({ type: VersioningType.URI, defaultVersion: '1' });
 
   const configService = app.get(ConfigService);
 
@@ -39,10 +39,10 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config, {
     extraModels: [SuccessResponseDto, CursorPaginatedDto, PaginatedDto],
   });
-  SwaggerModule.setup('/api/v1/docs', app, document, {
-    jsonDocumentUrl: '/api/v1/docs/swagger.json',
+  SwaggerModule.setup('/v1/docs', app, document, {
+    jsonDocumentUrl: '/v1/docs/swagger.json',
     swaggerOptions: {
-      url: '/api/v1/docs/swagger.json',
+      url: '/v1/docs/swagger.json',
       displayRequestDuration: true,
     },
   });
